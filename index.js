251136d42e325974d7c73e3c98dafafe4b7e3064
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-07 19:12:24 +0000

    Add Edge Case of test.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const Calculate = {
   factorial(n) {
     let fac = 1;
-    for (let i = n; i > 0; i--) {
-      fac *= i;
+    //standard check
+    if (n > 0) {
+      for (let i = n; i > 0; i--) {
+        fac *= i;
+      }
     }
+    //edge check is done aumtaically, if n = 0 then function returns 1
     return fac;
   },
 };
--- test/index_test.js
+++ test/index_test.js
@@ -7,11 +7,29 @@ describe("Calculate", () => {
       //setup
       const input = 5;
       const result = Calculate.factorial(input);
-
       //exercise
       const expected = 120;
+      //veify
+      assert.strictEqual(result, expected);
+    });
 
-      //verify
+    it("returns output of 3!", () => {
+      //setup
+      const input = 3;
+      const result = Calculate.factorial(input);
+      //exercise
+      const expected = 6;
+      //veify
+      assert.strictEqual(result, expected);
+    });
+
+    it("returns output of 0!", () => {
+      //setup
+      const input = 0;
+      const result = Calculate.factorial(input);
+      //exercise
+      const expected = 1;
+      //veify
       assert.strictEqual(result, expected);
     });
   });
